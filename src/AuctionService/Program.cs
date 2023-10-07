695f3773d13a56f9a05f4de2477a2d6fbea483c3
System Administrator <root@d207-023-168-084.wireless.sfu.ca>
2023-10-07 14:57:25 -0700

    Add AutoMapper profiles for Auction and Item mappings

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AuctionDbContext>(opt => {
   opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -18,5 +18,17 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+try
+{
+  DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+  Console.WriteLine(e);
+}
+
+
 
 app.Run();
